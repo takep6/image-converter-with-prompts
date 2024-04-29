3ce6247ecdad36c6cf0e37562aa8e4b20d0145c7
takep <msd2019tkhr@gmail.com>
2024-04-30 05:30:58 +0900

    プログレスバーを実装した

--- main.py
+++ main.py
@@ -2,26 +2,20 @@ import json
 import os
 
 import flet as ft
-from flet import (Card, Column, Container, CrossAxisAlignment, DragTarget,
-                  Dropdown, ElevatedButton, FilePicker, FilePickerResultEvent,
-                  Icon, MainAxisAlignment, Row, Slider, Switch, Text,
-                  TextField, alignment, colors, dropdown, icons)
+from flet import (Card, Column, Container, Dropdown, ElevatedButton,
+                  FilePicker, FilePickerResultEvent, Icon, MainAxisAlignment,
+                  ProgressBar, Row, Slider, Switch, Text, TextField, alignment,
+                  colors, dropdown, icons)
 
 import convert_to_webp as cv_webp
-
-"""
-TODO: jpgだけ非可逆圧縮ができず、エラーが発生してしまう。要修正
-"""
 
 
 def main(page):
     # page settings
     page.title = "Image Ext Converter"
-    page.vertical_alignment = MainAxisAlignment.CENTER
-    page.horizontal_alignment = CrossAxisAlignment.CENTER
     page.padding = 30
     page.window_width = 700
-    page.window_height = 800
+    page.window_height = 1000
 
     # json keys
     input_key = "input_path"
@@ -63,16 +57,19 @@ def main(page):
         init_lossless = False
 
     # descriptions
-    description01 = Text(value="AI生成画像のプロンプトを保持したまま画像ファイルの拡張子を変換します")
-    description02 = Text("ファイル形式はjpg, png, webpのみ対応")
+    description01 = Text("AI生成画像のプロンプトを保持したまま画像ファイルの拡張子を変換します")
+    description02 = Text("入力・出力ファイル形式はjpg, png, webpのみ対応")
+    description03 = Text("変換後の拡張子pngを選択した場合、圧縮率は無視されます")
+
+    font_bold = ft.FontWeight.BOLD
 
     # Input path field
-    input_path = TextField(label="Input Folder Path",
+    input_path = TextField(label="入力フォルダパス",
                            value=init_input_path,
                            width=500)
 
     # Output path field
-    output_path = TextField(label="Output Folder Path",
+    output_path = TextField(label="出力フォルダパス",
                             value=init_output_path,
                             width=500)
 
@@ -119,17 +116,24 @@ def main(page):
     )
 
     def set_comp_ratio_val(e):
-        comp_ratio_val_text.value = f"圧縮率: {str(int(e.control.value))}  %"
+        comp_val = int(e.control.value)
+        comp_ratio_val_text.value = f"圧縮率: {str(comp_val)}  %"
+        if comp_val <= 30:
+            compression_ratio.label = "ファイルサイズ 小"
+        elif comp_val <= 70:
+            compression_ratio.label = "ファイルサイズ 中"
+        else:
+            compression_ratio.label = "ファイルサイズ 大"
         page.update()
 
     # Compression ratio input
     compression_ratio = Slider(
         min=0, max=100,
-        label="Compression Ratio",
+        label="ファイルサイズ 大",
         value=init_compression_ratio, width=150, divisions=20,
         on_change=set_comp_ratio_val)
     comp_ratio_val_text = Text(
-        value=f"圧縮率: {init_compression_ratio} %", width=100)
+        value=f"圧縮率: {init_compression_ratio} %", width=120, weight=font_bold, color=colors.BLACK87, size=16)
 
     def toggle_lossless(e):
         compression_ratio.disabled = e.control.value
@@ -141,6 +145,8 @@ def main(page):
     # Log output
     log_output = TextField(
         label="Log", multiline=True, read_only=True, width=600)
+
+    progress_bar = ProgressBar(width=600, color=colors.AMBER_400)
 
     def run_compression(e):
         is_not_exist_path = input_path.value == "" or output_path.value == ""
@@ -157,7 +163,7 @@ def main(page):
         input_dir = input_path.value
         output_dir = output_path.value
         file_ext = file_types.value.lower()
-        is_lossless = lossless.value
+        is_lossless = True if file_ext == "png" else lossless.value
         ratio = 100 if is_lossless else int(compression_ratio.value)
 
         # save json
@@ -180,6 +186,8 @@ def main(page):
             log_output.value += f"Lossless: {is_lossless}\n"
         else:
             log_output.value += f"Compression Ratio: {ratio}%\n"
+        run_btn.disabled = True
+        page.add(progress_bar)
         page.update()
 
         # Actual compression logic goes here
@@ -190,55 +198,61 @@ def main(page):
             quality=ratio,
             lossless=is_lossless
         )
-
+        page.remove(progress_bar)
+        run_btn.disabled = False
         log_output.value += "Completed!"
         page.update()
 
     run_btn = ElevatedButton(
-        text="Run", on_click=run_compression, width=180, height=150)
-
-    def on_drop(e):
-        path_list = e.data.get("paths", [])
-        print(path_list)
+        text="実行", on_click=run_compression, width=180, height=150)
 
     page.add(
         Column(
             width=800,
             controls=[
+                Container(Text("画像圧縮変換ツール　アッシュ君", size=40, weight=font_bold),
+                          alignment=alignment.center, padding=20),
                 Container(Card(
                     Container(
                         Column([
                             description01,
                             description02,
+                            description03
                         ]), padding=20),
                     margin=10), alignment=alignment.center),
                 Container(
                     Column([
                         Row([
-                            DragTarget(content=input_path, on_accept=on_drop),
+                            input_path,
                             input_file_btn],
                             alignment=MainAxisAlignment.CENTER),
                         Row([output_path, output_file_btn],
                             alignment=MainAxisAlignment.CENTER),
                     ]), padding=20),
                 Row([
+                    Card(
+                        Container(
+                            Column([
+                                Row([
+                                    Text(value="変換後の拡張子", width=150,
+                                         weight=font_bold, color=colors.BLACK87),
+                                    file_types,
+                                ], width=250, alignment=MainAxisAlignment.START),
+                                Row([
+                                    Text(value="可逆圧縮", width=160,
+                                         weight=font_bold, color=colors.BLACK87),
+                                    lossless
+                                ], width=250, alignment=MainAxisAlignment.START),
+                                Row([
+                                    comp_ratio_val_text,
+                                    compression_ratio,
+                                ], width=250, alignment=MainAxisAlignment.START),
+                            ], height=200, alignment=MainAxisAlignment.SPACE_BETWEEN), padding=20, width=300)),
                     Container(
                         Column([
-                            Row([
-                                Text("変換後の拡張子"),
-                                file_types,
-                            ]),
-                            Row([
-                                Text(value="可逆圧縮"),
-                                lossless
-                            ]),
-                            Row([
-                                comp_ratio_val_text,
-                                compression_ratio,
-                            ]),
+                            run_btn
                         ]), padding=20),
-                    Container(run_btn, padding=20, alignment=alignment.center),
-                ], alignment=MainAxisAlignment.CENTER),
+                ], alignment=MainAxisAlignment.SPACE_AROUND),
                 Container(log_output, padding=20, alignment=alignment.center),
             ])
     ),
